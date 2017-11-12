2c7c1e0ae2d9e1df27e2170cbe4842a2ccc03281
Sam Hains <sdh@eml.cc>
2017-11-12 16:08:48 -0500

    dafda

--- script.js
+++ script.js
@@ -37,4 +37,24 @@ $( document ).ready(function() {
       }, rand);
   }());
 
+
+});
+
+window.addEventListener('load', function () {
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var source = audioCtx.createBufferSource();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'signal.mp3');
+    xhr.responseType = 'arraybuffer';
+    xhr.addEventListener('load', function (r) {
+        audioCtx.decodeAudioData(
+                xhr.response,
+                function (buffer) {
+                    source.buffer = buffer;
+                    source.connect(audioCtx.destination);
+                    source.loop = false;
+                });
+        source.start(0);
+    });
+    xhr.send();
 });
